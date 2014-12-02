d40d539d227bc21687fdaffb942be8de79bb8cb1
pitchapl <pitcha.pl@hotmail.com>
2014-12-02 15:58:13 +0700

    lasttry1

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -4,8 +4,9 @@ require "game"
 describe Game do
 	describe "#check_number" do
 		it "check number 3" do
-		x=lovemac(3)
-		expect(x).to eq ("love") 
+		i=Game.new(3)
+		x=i.lovemac;
+		expect(x).to eq("love") 
 	end
 	end
 end
