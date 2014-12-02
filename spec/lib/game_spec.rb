fb276c5b4c3e3cc7e60cfc1c66e26fe731f7ffe7
pitchapl <pitcha.pl@hotmail.com>
2014-12-02 15:27:32 +0700

    newtest2

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -3,7 +3,7 @@ require "game"
 
 describe Game do
 	describe "check number 3" do
-		injectinit { |mem, var|  }=lovemac(3)
-		expect(i).to eq ("love") 
+		x=lovemac(3)
+		expect(x).to eq ("love") 
 	end
 end
