ad7ffdf64bd68400dcc2e6878b3085520cdbfbb1
pitchapl <pitcha.pl@hotmail.com>
2014-12-02 15:42:21 +0700

    try2

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,19 @@
 class Game
+
+	(1..50).each do 
+		puts lovemac(number)
+	end
 	def lovemac(number)
-  case
-  when (number % 3 == 0) && (number % 5 == 0)
-    return "HateWindows"
-  when (number % 3 == 0)
-    return "Love"
-  when (number % 5 == 0)
-    return "Mac"
-  else
-    return number
-  end
+	  case
+	  when (number % 3 == 0) && (number % 5 == 0)
+	    return "HateWindows"
+	  when (number % 3 == 0)
+	    return "Love"
+	  when (number % 5 == 0)
+	    return "Mac"
+	  else
+	    return number
+	  end
 end
 end
 
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -2,8 +2,10 @@ require "spec_helper"
 require "game"
 
 describe Game do
-	describe "check number 3" do
+	describe "#check_number" do
+		it "check number 3" do
 		x=lovemac(3)
 		expect(x).to eq ("love") 
+	end
 	end
 end
